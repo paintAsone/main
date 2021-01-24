72d34e1d66f2e2e4043750e0e2a3e2bceae7431d
NATKAMONLAK <paintlpch@gmail.com>
2021-01-24 18:47:44 +0700

    switch case

---
+++ problems/pro_7/7_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char x;
+    cin>>x;
+    switch(x){
+        case 'A':
+            cout<<"Awesome";
+            break;
+        case 'B':
+            cout<<"Great";
+            break;
+        case 'C':
+            cout<<"Good";
+            break;
+        case 'F':
+            cout<<"Noob";
+            break;
+        default:
+            cout<<"Error: Invalid Input";
+            break;
+    }
+    return 0;
+}
