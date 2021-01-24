1b6a087b60b70facc71541fe6bfea51f0ab5610e
NATKAMONLAK <paintlpch@gmail.com>
2021-01-24 19:12:14 +0700

    -

---
+++ problems/pro_6/pro_6_3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 2, b = 3, c= 4;
+    c = b = 2 == a++;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+}
--- problems/pro_6_3.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<10/5%2+10/2*10<<endl;
-    cout<<2*2/2+2/2*2-2<<endl;
-    cout<<69%10%5%3;
-}
