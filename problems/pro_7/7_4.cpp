22555915eed4dcf23fd7efc15540651fa57cb863
NATKAMONLAK <paintlpch@gmail.com>
2021-01-24 19:19:35 +0700

    exsample

---
+++ problems/pro_7/7_4.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+/*int main(){
+    int x = 3;
+    switch(x){
+        case 7:
+            x*=2;
+            x++;
+            break;
+        case 3:
+            x++;
+        case 5:
+            x%=3;
+            x+=4;
+            break;
+        case 0:
+        case 2:
+            x/=2;
+            x--;
+            break;
+        default:
+            x++;
+    }
+    cout << "x = " << x;
+}*/
+
+int main(){
+    int num = 20;
+    if (num >= 10){
+        cout << "A";
+        cout << "B";
+    }else if(num >= 5)
+        cout << "C";
+    else
+        cout << "D";
+        cout << "E";
+    cout <<"F"; 
+}
